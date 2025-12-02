91a26e237f142bf435c8879a126e727e5d4b54b3
Victor Batista <victorbatista2013vb@gmail.com>
2025-12-02 14:26:40 -0300

    Put GeneratedValue in prontuario and usuario

--- src/main/java/com/example/springboot/model/prontuario.java
+++ src/main/java/com/example/springboot/model/prontuario.java
@@ -2,11 +2,30 @@ package com.example.springboot.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.format.annotation.DateTimeFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 
+@Entity
+@Table(name = "prontuarios")
 public class Prontuario {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long idProntuario;
+
+    @Id
     private Long idPaciente;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Column(name="data_consulta", columnDefinition = "DATE")
     private LocalDate dataConsulta;
+
+    @Column(length = 100)
     private String descricao;   
     
     public Long getIdProntuario() {
--- src/main/java/com/example/springboot/model/usuario.java
+++ src/main/java/com/example/springboot/model/usuario.java
@@ -1,18 +1,36 @@
 package com.example.springboot.model;
 
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
-
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
-    private UUID id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
     private String login;
+
+    @Column(nullable = false, length = 12)
     private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     private Funcao funcao;
 
-    public UUID getId() {
+    public Long getId() {
         return id;
     }
-    public void setId(UUID id) {
+    public void setId(Long id) {
         this.id = id;
     }
     public String getLogin() {
