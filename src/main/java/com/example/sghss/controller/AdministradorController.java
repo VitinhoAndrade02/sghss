6e2c534edac56a5abde6de343f3db8d8a6ecec0c
Victor Batista <victorbatista2013vb@gmail.com>
2025-12-09 00:07:39 -0300

    commit

---
+++ src/main/java/com/example/sghss/controller/AdministradorController.java
@@ -0,0 +1,28 @@
+package com.example.sghss.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.sghss.bo.AdministradorBO;
+import com.example.sghss.model.Administrador;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/administrador")
+public class AdministradorController {
+
+    @Autowired
+    private AdministradorBO bo;
+
+    @RequestMapping(value="/novo", method= RequestMethod.GET)
+    public ModelAndView novo (ModelMap model) {
+        model.addAttribute("administrador", new Administrador());
+        return new ModelAndView("/administrador/formulario", model);
+    }
+
+    
+}
