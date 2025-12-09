acb9cd2dc06a5535a0b9339a71cc79ba75ada94f
Victor Batista <victorbatista2013vb@gmail.com>
2025-12-09 00:14:57 -0300

    change

--- src/main/java/com/example/sghss/controller/AdministradorController.java
+++ src/main/java/com/example/sghss/controller/AdministradorController.java
@@ -4,15 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.sghss.bo.AdministradorBO;
 import com.example.sghss.model.Administrador;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-@RequestMapping("/administrador")
+@RequestMapping("/administradores")
 public class AdministradorController {
 
     @Autowired
