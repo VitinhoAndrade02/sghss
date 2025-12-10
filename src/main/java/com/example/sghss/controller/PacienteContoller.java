1dad78496513759fe2f6cc8dfd2982a0765b16c7
Victor Batista <victorbatista2013vb@gmail.com>
2025-12-09 22:45:19 -0300

    best change

--- src/main/java/com/example/sghss/controller/PacienteContoller.java
+++ src/main/java/com/example/sghss/controller/PacienteContoller.java
@@ -3,14 +3,13 @@ package com.example.sghss.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 
 import com.example.sghss.bo.PacienteBO;
 import com.example.sghss.model.Paciente;
@@ -38,7 +37,7 @@ public class PacienteContoller {
             return "paciente/formulario";
         }
 
-        if (paciente.getIdPaciente() != null) {
+        if (paciente.getIdPaciente() == null) {
             bo.create(paciente);
         } 
 
--- src/main/java/com/example/sghss/model/Paciente.java
+++ src/main/java/com/example/sghss/model/Paciente.java
@@ -15,8 +15,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "pacientes")
