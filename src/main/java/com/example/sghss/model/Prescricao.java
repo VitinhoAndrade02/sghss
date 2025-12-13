d002382cbe9ba55558899cddea65f7d837a4cc24
Victor Batista <victorbatista2013vb@gmail.com>
2025-12-12 22:49:39 -0300

    feat: Enhance UI and functionality for various templates including Prescrição, Teleconsulta, and Unidade

--- src/main/java/com/example/sghss/model/Prescricao.java
+++ src/main/java/com/example/sghss/model/Prescricao.java
@@ -1,6 +1,15 @@
 package com.example.sghss.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
@@ -22,9 +31,6 @@ public class Prescricao {
     @Enumerated(EnumType.STRING)
     @NotNull(message = "Informe o tipo da prescrição")
     private TipoPrescricao tipoPrescricao;
-    
-    //Data da emisssão
-    //Medicamentos
 
     @Column(length = 500)
     private String observacao;
