2aae4e18c722faca47b3db68dcaa01fac5eb590d
Victor Batista <victorbatista2013vb@gmail.com>
2025-12-19 11:15:46 -0300

    Auditoria changes

--- src/main/java/com/example/sghss/model/AuditoriaListener.java
+++ src/main/java/com/example/sghss/model/AuditoriaListener.java
@@ -1,18 +1,19 @@
 package com.example.sghss.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.example.sghss.bo.LogAuditoriaBO;
+
 import jakarta.persistence.PostPersist;
 import jakarta.persistence.PostUpdate;
 import jakarta.persistence.PreRemove;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 @Component
 public class AuditoriaListener {
 
     private static LogAuditoriaBO logBO;
 
-    // O Spring chama este método logo que o sistema inicia para "entregar" o BO
     @Autowired
     public void init(LogAuditoriaBO bo) {
         AuditoriaListener.logBO = bo;
@@ -41,7 +42,6 @@ public class AuditoriaListener {
                 log.setEntidade(obj.getClass().getSimpleName());
                 log.setUsuario("Sistema");
 
-                // Captura o ID da entidade via reflexão
                 java.lang.reflect.Method method = obj.getClass().getMethod("getId");
                 log.setEntidadeId((Long) method.invoke(obj));
 
